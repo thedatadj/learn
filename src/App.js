38c6f0f7758f5a31cf174c37217ef0362d12fa87
unknown <edavid.rd@gmail.com>
2024-04-12 12:50:18 -0400

    initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <nav>
+        <h1 id='cs'>Computer Science</h1>
+        <h1 id='math'>Mathematics</h1>
+      </nav>
+    </>
+  );
+}
+
+export default App;
